4731a76d7b87740c2af0296cf23e5849abf9eeec
RiaFitriyani <riafitriani0090@gmail.com>
2023-03-14 07:35:27 +0700

    tugas3

---
+++ pertemuan3/tugas 1/home.php
@@ -0,0 +1,21 @@
+
+
+<body>
+    <div class="container mt-5">
+        <header role="banner">
+            <h2>Student Activity Score - STTNF</h2>
+            <a href="">Home</a> | <a href="">Form Nilai</a> | <a href="">Data Nilai</a> | <a href="">Login</a>
+            <hr />
+        </header>
+				<h1>Welcome Home!</h1>
+				<p>
+				    <!--
+				        Ria Fitriyani - 0110122062 - si10 
+				    -->
+				</p>
+				<footer class="text-center py-3 bg-light mt-5">
+        <p>Created by <a href="#">Your Name</a> &copy; <?php echo date('Y') ?></p>
+    </footer>
+</body>
+
+</html>
